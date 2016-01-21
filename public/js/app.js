132cd7256b1f325af3d4fceaa7e5bbdabf7aa326
Daniel <daniel.vernberg@gmail.com>
2016-01-21 12:07:12 +0100

    changes

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular
+  .module('app', [
+    'ui.router'
+  ])
+  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'templates/index.html'
+      })
+
+
+  }]);
