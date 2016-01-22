812cee2483be6c4225a48e46af6ac4ad4404caa8
Daniel <daniel.vernberg@gmail.com>
2016-01-22 14:37:01 +0100

    correct http get

--- public/js/controllers/singleAdCtrl.js
+++ public/js/controllers/singleAdCtrl.js
@@ -5,10 +5,10 @@ angular
     .controller('singleAdCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
 
       // Create variable from param
-      // var ad = $stateParams.singleAd;
-      var ad = 2819163;
+      var ad = $stateParams.adID;
 
-      $http.get('http://localhost:1339/api/singleAd/' + ad)
+
+      $http.get('http://localhost:1339/api/singleAd/' + ad + '')
       .then(function(data) {
         $scope.adDetails = data;
       })
