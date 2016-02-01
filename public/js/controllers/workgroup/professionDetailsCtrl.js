b9896672cfc50ebca73d7e4f1fe36ac594630921
Daniel <daniel.vernberg@gmail.com>
2016-02-01 20:42:25 +0100

    LocationState
    Inject locationState in ctrl. attaching a scope function to set
    location state. Calling SetLocation function from template which sets
    location name

--- public/js/controllers/workgroup/professionDetailsCtrl.js
+++ public/js/controllers/workgroup/professionDetailsCtrl.js
@@ -2,7 +2,7 @@ angular
   .module('app')
 
     // Controller to view imdb id
-    .controller('professionDetailsCtrl', ['$scope', '$http', '$stateParams', 'WorkGroupState', function($scope, $http, $stateParams, WorkGroupState) {
+    .controller('professionDetailsCtrl', ['$scope', '$http', '$stateParams', 'WorkGroupState', 'LocationState', function($scope, $http, $stateParams, WorkGroupState, LocationState) {
 
       // Create variable from param
       var id = $stateParams.professionID;
@@ -14,6 +14,14 @@ angular
       .then(function(response) {
         $scope.result = response;
         $scope.ads = response.data.body.matchningslista.matchningdata;
+        console.log(response);
       })
+
+      // set locationState upon clicking a county
+      $scope.setLocation = function(location) {
+        console.log('du klickade på', location);
+        LocationState.setLocation(location);
+        console.log("Location är nu:" + location);
+      }
 
     }])
