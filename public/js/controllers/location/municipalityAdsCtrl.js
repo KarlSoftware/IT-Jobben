a8769183f422eaf63346cc5d11137d1e2e01462d
Daniel <daniel.vernberg@gmail.com>
2016-02-07 19:54:23 +0100

    conflict

--- public/js/controllers/location/municipalityAdsCtrl.js
+++ public/js/controllers/location/municipalityAdsCtrl.js
@@ -2,6 +2,7 @@ angular
   .module('app')
 
     // Controller to get all municipalities in a county
+<<<<<<< HEAD
     .controller('municipalityAdsCtrl', [
       '$scope',
       '$http',
@@ -35,5 +36,32 @@ angular
 
 
 
+=======
+    .controller('municipalityAdsCtrl', ['$scope', '$http', '$stateParams', 'LocationState', 'BreadcrumbState', function($scope, $http, $stateParams, LocationState, BreadcrumbState) {
+
+      // Create variable from param
+      var municipalityID = $stateParams.municipalityID;
+
+
+      // fetch current municipality name and county name
+      $scope.currentMunicipality = LocationState.getMunicipality();
+      $scope.currentCounty = LocationState.getCounty();
+
+      // fetch current municipality and county breadcrumbs
+      $scope.currentMunicipalityBreadcrumb = BreadcrumbState.getMunicipalityBreadcrumb();
+      $scope.currentCountyBreadcrumb = BreadcrumbState.getCountyBreadcrumb();
+
+      // make http req
+      $http.get('http://localhost:1339/location/municipality/' + municipalityID)
+      .then(function(response) {
+        $scope.howManyAds = response.data.body.matchningslista.antal_platsannonser_exakta;
+        $scope.howManyAdsNear = response.data.body.matchningslista.antal_platsannonser_narliggande;
+        console.log($scope.howManyAdsNear);
+        $scope.ads = response.data.body.matchningslista.matchningdata;
+        console.log(response);
+        // $scope.workgroup = response.data.body.soklista.sokdata;
+      })
+
+>>>>>>> parent of 92aa77b... infinity scroll
 
     }])
