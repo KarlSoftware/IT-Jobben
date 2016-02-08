63dc504a78fe4f092bb0fce9841e99b6a49b6ebd
Daniel <daniel.vernberg@gmail.com>
2016-02-08 13:03:01 +0100

    moved some logic over to the controller on places it seemed right

--- public/js/controllers/singleAd/singleAdCtrl.js
+++ public/js/controllers/singleAd/singleAdCtrl.js
@@ -34,17 +34,54 @@ angular
         $scope.ansokan =        response.data.body.platsannons.ansokan;
         $scope.arbetsplats =    response.data.body.platsannons.arbetsplats;
         $scope.krav =           response.data.body.platsannons.krav;
+        $scope.driverslicenseList = response.data.body.platsannons.krav.korkortslista;
         $scope.villkor =        response.data.body.platsannons.villkor;
         console.log($scope.adDetails);
 
         // do logic depending on what the response contains
+        // logic for sista ansökningsdag
         if ($scope.ansokan.sista_ansokningsdag) {
           if ($scope.ansokan.sista_ansokningsdag == '') {
-            $scope.sista_ansokningsdagDone = 'ingen info';
+            $scope.sista_ansokningsdag = 'ingen info';
           } else {
-            $scope.sista_ansokningsdagDone = moment($scope.ansokan.sista_ansokningsdag).format("dddd, Do MMMM, h:mm");
+            $scope.sista_ansokningsdag = moment($scope.ansokan.sista_ansokningsdag).format("dddd, Do MMMM, h:mm");
           }
+        } else {
+          $scope.sista_ansokningsdag = 'ingen info';
         }
-      })
+
+        // logic for referens
+        if ($scope.ansokan.referens) {
+          if ($scope.ansokan.referens == '') {
+            $scope.referens = 'ingen info';
+          } else {
+            $scope.referens = $scope.ansokan.referens;
+          }
+        } else {
+          $scope.referens = 'ingen info';
+        }
+
+        // logic for email
+        if ($scope.ansokan.epostadress) {
+          if ($scope.ansokan.epostadress == '') {
+            $scope.epost = 'ingen info';
+          } else {
+            $scope.epost = $scope.ansokan.epostadress;
+          }
+        } else {
+          $scope.epost = 'ingen info';
+        }
+
+        // logic for egenbil
+        if ($scope.krav.egenbil) {
+            $scope.bil = 'Annonsen kräver egen bil';
+          } else {
+            $scope.bil = 'Inget krav om egen bil';
+          }
+        
+
+
+
+      }) // end of http then
 
     }])
