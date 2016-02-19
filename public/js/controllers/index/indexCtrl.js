26fe910571c05ef54930fabc7b573754e554c4eb
Daniel <daniel.vernberg@gmail.com>
2016-02-19 12:12:16 +0100

    created controller for index page and set up typed.js

---
+++ public/js/controllers/index/indexCtrl.js
@@ -0,0 +1,28 @@
+angular
+  .module('app')
+
+    // Controller to view all counties
+    .controller('indexCtrl', [
+      '$scope',
+      '$http',
+      '$stateParams',
+      function($scope, $http, $stateParams) {
+
+
+        console.log('hello from index');
+
+        $(".typed").typed({
+          strings: ["Frontend-utvecklare",
+                    "Dataoperatör",
+                    "Användbarhetsdesigner",
+                    "Nätverkstekniker",
+                    "Civilingenjör",
+                    "Programmerare"
+                    ],
+          typeSpeed: 0.6,
+          backDelay: 1000
+        });
+
+
+
+    }])
