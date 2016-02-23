17553ba573b3cf6ea582391070206965dfaf7199
Daniel <daniel.vernberg@gmail.com>
2016-02-23 22:50:02 +0100

    updated gulp with a task to concat js files for dev folder and for dist folder. Added those two to default watch

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,34 @@ gulp.task('default', function() {
 /*
 * Concat and uglify .js files
 */
-gulp.task('javascript', function() {
-  return gulp.src(['public/js/*.js', 'public/js/controllers/*/*.js'])
+gulp.task('js-dist', function() {
+  return gulp.src(['public/js/app.js', 'public/js/controllers/*/*.js'])
     .pipe(concat('itjobben.js', {
     }))
     .pipe(jsmin())
     .pipe(gulp.dest('./dist/js'));
 });
+
+/*
+* Concat and uglify .js files
+* Outputs in public directory for use while development
+*/
+gulp.task('js-dev', function() {
+  return gulp.src(['public/js/app.js', 'public/js/controllers/*/*.js'])
+    .pipe(concat('itjobben.js', {
+    }))
+    .pipe(gulp.dest('./public/js'));
+});
+
+/*
+* Gulp watch
+*/
+gulp.task('watch', function() {
+  gulp.watch(['public/js/app.js', 'public/js/controllers/*/*.js'], ['js-dist', 'js-dev']);
+})
+
+/*
+* Gulp default
+* this will do everything
+*/
+gulp.task('default', ['js-dev', 'js-dist', 'watch'])
