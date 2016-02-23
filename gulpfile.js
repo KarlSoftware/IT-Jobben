2434a6886cf2ea6b29a1071aa2421a434d1c0104
Daniel <daniel.vernberg@gmail.com>
2016-02-23 23:01:52 +0100

    minify html templates and watch them

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
-    jsmin = require('gulp-jsmin');
+    jsmin = require('gulp-jsmin'),
+    htmlmin = require('gulp-htmlmin');;
 
 gulp.task('default', function() {
   console.log('Gulp working!');
@@ -32,10 +33,38 @@ gulp.task('js-dev', function() {
 });
 
 /*
+* Minify html index page
+*/
+gulp.task('minify-html-index', function() {
+  return gulp.src('public/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist'))
+});
+
+/*
+* Minify html template root page
+*/
+gulp.task('minify-html-template-root', function() {
+  return gulp.src('public/templates/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist/templates/'))
+});
+
+/*
+* Minify html template root page
+*/
+gulp.task('minify-html-templates-folders', function() {
+  return gulp.src('public/templates/*/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist/templates/'))
+});
+
+/*
 * Gulp watch
 */
 gulp.task('watch', function() {
   gulp.watch(['public/js/app.js', 'public/js/controllers/*/*.js'], ['js-dist', 'js-dev']);
+  gulp.watch(['public/index.html', 'public/templates/*', 'public/templates/*/*.html'], ['minify-html-index', 'minify-html-template-root', 'minify-html-templates-folders'])
 })
 
 /*
