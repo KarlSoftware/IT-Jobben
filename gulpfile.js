a8da96bbb433009a9e5be0ddbf858c865d471572
Daniel <daniel.vernberg@gmail.com>
2016-02-23 21:36:10 +0100

    concat and minify javascript files in place

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    jsmin = require('gulp-jsmin');
 
 gulp.task('default', function() {
-
   console.log('Gulp working!');
 
+});
+
+
+/*
+* Concat and uglify .js files
+*/
+gulp.task('javascript', function() {
+  return gulp.src(['public/js/*.js', 'public/js/controllers/*/*.js'])
+    .pipe(concat('itjobben.js', {
+    }))
+    .pipe(jsmin())
+    .pipe(gulp.dest('./dist/js'));
 });
