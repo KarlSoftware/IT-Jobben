cdda6384a743e7ad5e384d3eda3cf3058d9cce55
Daniel <daniel.vernberg@gmail.com>
2016-02-24 10:09:45 +0100

    moved inline script code to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
+/*
+* jQuery code to properly collapse the navbar
+*/
+$(document).on('click','.navbar-collapse.in',function(e) {
+  if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+      $(this).collapse('hide');
+  }
+});
+
 angular
   .module('app', [
     'ui.router',
