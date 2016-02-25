48d513f2f329bfce38d4f35859a5e3f72c3cfaf2
Daniel <daniel.vernberg@gmail.com>
2016-02-25 17:31:23 +0100

    utilizing $state for better searching
    refresh search content even if you search with the search bar in navbar
    if you're on search result page

--- app/js/controllers/search/searchBarCtrl.js
+++ app/js/controllers/search/searchBarCtrl.js
@@ -7,17 +7,32 @@ angular
       '$http',
       '$stateParams',
       '$location',
-      function($scope, $http, $stateParams, $location) {
+      '$state',
+      function($scope, $http, $stateParams, $location, $state) {
 
+        // scope function to set active state. Used to assign active class to navbar links
         $scope.isActive = function (viewLocation) {
           return viewLocation === $location.path().substring(0, 5);
         };
 
-
         $scope.doSearch = function () {
           // set sessionStorage
-          sessionStorage.setItem("searchTerm", $scope.searchterm);
+          sessionStorage.setItem("searchTerm", $scope.navbarSearchTerm);
+          console.log($scope.navbarSearchTerm)
           sessionStorage.setItem("paginationSearch", '1');
+
+          // go to searchresults page if not already there
+          if ($state.current.name != 'searchresults') {
+            $state.go('searchresults'); // change $state
+          }
+
+          // reload view if current $state is searchResults
+          if ($state.current.name == 'searchresults') {
+            $state.reload(); // reload $state
+          }
+
         };
+
+
 
     }]);
