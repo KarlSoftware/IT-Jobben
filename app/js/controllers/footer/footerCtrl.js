7a106e18e5161ebde81d8dcbd4ac414f70279b17
Daniel <daniel.vernberg@gmail.com>
2016-02-27 16:26:31 +0100

    comments

--- app/js/controllers/footer/footerCtrl.js
+++ app/js/controllers/footer/footerCtrl.js
@@ -1,12 +1,13 @@
 angular
   .module('app')
 
-    // Controller to view all counties
+    // Controller for footer
     .controller('footerCtrl', [
       '$scope',
       '$http',
       '$stateParams',
       function($scope, $http, $stateParams) {
 
+        // get current date to use in copyright context
         $scope.date = new Date();
     }]);
