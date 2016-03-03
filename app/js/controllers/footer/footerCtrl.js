66ca78bac2012a7084d62b1f1c482aad00b896f3
Daniel <daniel.vernberg@gmail.com>
2016-03-03 11:02:15 +0100

    save current url path to sessionStorage

--- app/js/controllers/footer/footerCtrl.js
+++ app/js/controllers/footer/footerCtrl.js
@@ -5,8 +5,14 @@ angular
     .controller('footerCtrl', [
       '$scope',
       '$http',
-      function($scope, $http) {
+      '$location',
+      function($scope, $http, $location) {
 
         // get current date to use in copyright text
         $scope.date = new Date();
+
+        // function to save current $location.path in sessionStorage. For usage later on about route
+        $scope.setURL = function() {
+          sessionStorage.setItem('previousURL', $location.path());
+        }
     }]);
