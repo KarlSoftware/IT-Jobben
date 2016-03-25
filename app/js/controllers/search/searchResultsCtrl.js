a345a56aaeb3cfa1d513d0d6438ad4bc9c3a39bc
Daniel <daniel.vernberg@gmail.com>
2016-03-25 12:01:43 +0100

    correct indentation

--- app/js/controllers/search/searchResultsCtrl.js
+++ app/js/controllers/search/searchResultsCtrl.js
@@ -8,62 +8,24 @@ angular
       '$rootScope',
       function($scope, $http, $rootScope) {
 
-      // set page header
-      $rootScope.header = 'Sökning - IT Jobben';
-
-      $scope.searchTerm = sessionStorage.getItem("searchTerm");
-
-      // fetch current pagination page. Defaults to 1 if pagination sessionStorage isn't set yet
-      if (sessionStorage.getItem("paginationSearch") === null) {
-        $scope.pagination = '1';
-      } else {
-        $scope.paginationPage = sessionStorage.getItem("paginationSearch");
-      }
-
-      // set empty array to fill up with ads matching above 75%
-      var adsAbove75 = [];
-
-      $http.get('api/search/' + sessionStorage.getItem("searchTerm"), {
-        ignoreLoadingBar: false,
-      })
-      .then(function(response) {
-        console.log(response);
-        $scope.ads = response.data.matchningslista.matchningdata;
-        // loop through ads to get 100% matches
-        for (i = 0; i < $scope.ads.length; i++) {
-          if ($scope.ads[i].relevans == 100) {
-            adsAbove75.push($scope.ads[i]);
-          }
-        }
-
-        $scope.ads75 = adsAbove75;
-      }); // end of then
-
-      // dir-pagination-controls function to change current pagination page
-      $scope.changePagination = function(newPageNumber, oldPageNumber) {
-        $scope.paginationPage = newPageNumber;
-        // set sessionStorage
-        sessionStorage.setItem("paginationSearch", newPageNumber);
-        $scope.paginationPage = sessionStorage.getItem("paginationSearch");
-      };
 
 
+        // set page header
+        $rootScope.header = 'Sökning - IT Jobben';
 
-      // function to search again
-      $scope.search = function () {
-
-        // reset results array
-        adsAbove75 = [];
-
-        // reset pagination page to Defaults
-        $scope.paginationPage = '1';
-
-        // set searchterm
-        sessionStorage.setItem("searchTerm", $scope.searchterm);
+        // set searchTerm to scope
         $scope.searchTerm = sessionStorage.getItem("searchTerm");
 
+        // fetch current pagination page. Defaults to 1 if pagination sessionStorage isn't set yet
+        if (sessionStorage.getItem("paginationSearch") === null) {
+          $scope.pagination = '1';
+        } else {
+          $scope.paginationPage = sessionStorage.getItem("paginationSearch");
+        }
 
-        // do http req with new search term
+        // set empty array to fill up with ads matching above 75%
+        var adsAbove75 = [];
+
         $http.get('api/search/' + sessionStorage.getItem("searchTerm"), {
           ignoreLoadingBar: false,
         })
@@ -78,9 +40,50 @@ angular
           }
 
           $scope.ads75 = adsAbove75;
-        });
+        }); // end of then
 
-      };
+        // dir-pagination-controls function to change current pagination page
+        $scope.changePagination = function(newPageNumber, oldPageNumber) {
+          $scope.paginationPage = newPageNumber;
+          // set sessionStorage
+          sessionStorage.setItem("paginationSearch", newPageNumber);
+          $scope.paginationPage = sessionStorage.getItem("paginationSearch");
+        };
+
+
+
+        // function to search again
+        $scope.search = function () {
+
+          // reset results array
+          adsAbove75 = [];
+
+          // reset pagination page to Defaults
+          $scope.paginationPage = '1';
+
+          // set searchterm
+          sessionStorage.setItem("searchTerm", $scope.searchterm);
+          $scope.searchTerm = sessionStorage.getItem("searchTerm");
+
+
+          // do http req with new search term
+          $http.get('api/search/' + sessionStorage.getItem("searchTerm"), {
+            ignoreLoadingBar: false,
+          })
+          .then(function(response) {
+            console.log(response);
+            $scope.ads = response.data.matchningslista.matchningdata;
+            // loop through ads to get 100% matches
+            for (i = 0; i < $scope.ads.length; i++) {
+              if ($scope.ads[i].relevans == 100) {
+                adsAbove75.push($scope.ads[i]);
+              }
+            }
+
+            $scope.ads75 = adsAbove75;
+          });
+
+        };
 
 
     }]); // end of controller
