e2185e610f799b911ee71b1f12d1c722a58d9b12
Daniel <daniel.vernberg@gmail.com>
2016-07-31 12:25:13 +0200

    removed left over files since UX change and split child controllers into separate file for better overview and structure

--- app/js/controllers/location/countiesCtrl.js
+++ app/js/controllers/location/countiesCtrl.js
@@ -75,19 +75,5 @@ angular
           $scope.paginationPage = newPageNumber;
           $location.search({län: $location.search().län, kommun: $location.search().kommun, sida: newPageNumber});
         };
+
     }])
-
-    // Child Controller to get number of ads in a county
-    .controller('countyChildCtrl', [
-      '$scope',
-      '$http',
-      function($scope, $http) {
-
-        $http.get('location/match/county/' + $scope.county.id +'', {
-          ignoreLoadingBar: false
-        })
-        .then(function(response) {
-          $scope.adsInCounty = response.data.body.matchningslista.antal_platsannonser_exakta;
-        });
-
-    }]);
